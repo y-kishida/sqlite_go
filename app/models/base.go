6d2b1458b84d6630d0d2cdf467e0db8194613384
y-kishida <k.yoshikazu1221@gmail.com>
2022-12-23 11:04:40 +0900

    feat: todos table

--- app/models/base.go
+++ app/models/base.go
@@ -15,7 +15,10 @@ var Db *sql.DB
 
 var err error
 
-const tableNameUser = "users"
+const (
+	tableNameUser = "users"
+	tableNameTodo = "todos"
+)
 
 func init() {
 	Db, err = sql.Open(config.Config.SQLDriver, config.Config.DbName)
@@ -31,6 +34,16 @@ func init() {
 		created_at DATETIME)`, tableNameUser)
 
 	Db.Exec(cmdU)
+
+	cmdT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		uuid STRING NOT NUll UNIQUE,
+		name STRING,
+		email STRING,
+		password STRING,
+		created_at DATETIME)`, tableNameTodo)
+
+	Db.Exec(cmdT)
 }
 
 func createUUID() (uuidobj uuid.UUID) {
