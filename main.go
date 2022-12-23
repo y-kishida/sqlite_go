94e7fbf3c3fb787364b4bb728191bc4885468fc8
y-kishida <k.yoshikazu1221@gmail.com>
2022-12-23 19:07:18 +0900

    feat: get todos

--- main.go
+++ main.go
@@ -6,16 +6,8 @@ import (
 )
 
 func main() {
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
-	u.CreateUser()
-
-	user, _ := models.GetUser(2)
-	user.CreateTodo("First Todo")
-	fmt.Println(models.Db)
-
-	todo, _ := models.GetTodo(1)
-	fmt.Println(todo)
+	todos, _ := models.GetTodos()
+	for i := range todos {
+		fmt.Println(todos[i])
+	}
 }
